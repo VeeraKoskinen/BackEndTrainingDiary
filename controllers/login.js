391ec13382270fb93c4c34383e00e9e5770de177
VeeraKoskinen <veera.koskinen@ren.to>
2018-04-27 10:26:11 +0300

    Little fix in user data

--- controllers/login.js
+++ controllers/login.js
@@ -24,7 +24,10 @@ loginRouter.post('/', async (request, response) => {
   const token = jwt.sign(userForToken, process.env.SECRET)
   console.log(token)
 
-  response.status(200).send({ token, username: user.username, name: user.name })
+  console.log("Käyttäjän id, joka tulisilähettää:")
+  console.log(user._id)
+
+  response.status(200).send({ token, username: user.username, name: user.name, id: user._id })
 })
 
 module.exports = loginRouter
